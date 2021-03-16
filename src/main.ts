53d65991eb148b8dd89231d395108eb869848358
yoon-chaejin <kevin3st@naver.com>
2021-03-16 22:43:13 +0900

    feat: install helmet

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: {'origin': 'http://localhost:3100',}});
+  app.use(helmet())
   await app.listen(3101);
 }
 bootstrap();
