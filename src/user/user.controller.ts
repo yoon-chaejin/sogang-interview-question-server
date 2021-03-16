317ac156f6adae96bd925c4b0a81568460a74753
yoon-chaejin <kevin3st@naver.com>
2021-03-16 23:07:46 +0900

    fix: add async-await to all controllers and services

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,8 +8,8 @@ export class UserController {
     constructor(private readonly userService: UserService) {}
 
     @Get()
-    findAll(): Promise<User[]> {
-        return this.userService.findAll();
+    async findAll(): Promise<User[]> {
+        return await this.userService.findAll();
     }
     
     @Post()
