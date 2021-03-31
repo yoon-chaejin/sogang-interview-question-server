72a64dcf016e788fd1b77f1fa2c75d18941414e0
yoon-chaejin <kevin3st@naver.com>
2021-04-01 01:22:17 +0900

    feat: allow multiple cross-origins

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as helmet from 'helmet';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: {'origin': ['http://localhost:3100', 'http://13.209.6.144', 'http://www.sogangtree.ml', 'https://www.sogangtree.ml'],}});
+  const app = await NestFactory.create(AppModule, { cors: {'origin': ['http://localhost:3100', 'http://13.209.6.144', 'http://*.sogangtree.ml', 'https://*.sogangtree.ml'],}});
   const configService = app.get(ConfigService);
   app.use(helmet())
   await app.listen(configService.get('port'));
