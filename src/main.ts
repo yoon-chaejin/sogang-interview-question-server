770fd9b23d7f0cce620d19654690cd34fbf97fc2
Ubuntu <ubuntu@ip-172-31-34-171.ap-northeast-2.compute.internal>
2021-04-01 11:32:47 +0000

    fix: cors error

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import * as helmet from 'helmet';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: {'origin': ['http://localhost:3100', 'http://13.209.6.144', 'http://*.sogangtree.ml', 'https://*.sogangtree.ml'],}});
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const configService = app.get(ConfigService);
   app.use(helmet())
   await app.listen(configService.get('port'));
